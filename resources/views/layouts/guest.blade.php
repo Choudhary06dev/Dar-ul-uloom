61a006012fb7806d93a5576448a81afe1c28c3eb
unknown <choudharyamjad04@gmail.com>
2026-03-31 18:42:18 +0500

    login pages

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -24,8 +24,6 @@
             }
             html, body {
                 width: 100%;
-                height: 100%;
-                overflow: hidden !important;
                 margin: 0;
                 padding: 0;
             }
